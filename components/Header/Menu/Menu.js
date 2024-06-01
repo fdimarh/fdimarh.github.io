271cbee3cc49fd9bd58f7345e378e6a736cd698e
Fajar Dimar Habibi <fajardimarh@gmail.com>
2024-06-01 21:18:48 +0700

    remove unneeded content

--- components/Header/Menu/Menu.js
+++ components/Header/Menu/Menu.js
@@ -4,6 +4,7 @@ import { MENULINKS } from "../../../constants";
 const Menu = () => {
   useEffect(() => {
     const anchorNodes = document.querySelectorAll('a[href^="#"]');
+
     anchorNodes.forEach((el) => {
       el.addEventListener("click", () => {
         const checkbox = document.querySelector(".checkbox-toggle");
@@ -13,24 +14,24 @@ const Menu = () => {
   }, []);
 
   return (
-    <div className="menu fixed top-0 left-0 w-full h-full overflow-hidden invisible pointer-events-none flex items-center justify-center">
-      <div className="flex-none overflow-hidden flex items-center justify-center">
-        <div className="text-center opacity-0 overflow-y-auto overflow-x-hidden flex flex-none justify-center items-center max-h-screen">
-          <ul className="list-none py-4 px-0 m-0 block max-h-screen">
-            {MENULINKS.map((el) => (
-              <li className="p-0 m-6 text-2xl block" key={el.name}>
-                <a
-                  className="link relative inline font-mono font-bold text-5xl duration-300 hover:no-underline"
-                  href={`#${el.ref}`}
-                >
-                  {el.name}
-                </a>
-              </li>
-            ))}
-          </ul>
+      <div className="menu fixed top-0 left-0 w-full h-full overflow-hidden invisible pointer-events-none flex items-center justify-center">
+        <div className="flex-none overflow-hidden flex items-center justify-center">
+          <div className="text-center opacity-0 overflow-y-auto overflow-x-hidden flex flex-none justify-center items-center max-h-screen">
+            <ul className="list-none py-4 px-0 m-0 block max-h-screen">
+              {MENULINKS.map((el) => (
+                  <li key={el.name} className="p-0 m-6 text-2xl block">
+                    <a
+                        className="link relative inline font-mono font-bold text-5xl duration-300 hover:no-underline"
+                        href={`#${el.ref}`}
+                    >
+                      {el.name}
+                    </a>
+                  </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
